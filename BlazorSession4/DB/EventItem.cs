bddbefd2f7128df033c0d77b8ac37eb2666da952
Alex Pushkin <ifc@suz-ppk.ru>
2025-02-25 15:59:32 +1000

    А че он не все залил

---
+++ BlazorSession4/DB/EventItem.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorSession4.DB;
+
+public partial class EventItem
+{
+    public ulong Uid { get; set; }
+
+    public string Summary { get; set; } 
+
+    public string Description { get; set; } 
+
+    public DateTime DtStamp { get; set; }
+    public DateTime DtStart { get; set; }
+    public DateTime DtEnd { get; set; }
+
+    public string Location { get; set; }
+    public string Organizer { get; set; }
+}
